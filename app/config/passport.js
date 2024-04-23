fa7be3c0a03c917a8f0d47b727d629ab4c440227
mrinal2002 <mrinal2002patra@gmail.com>
2024-04-23 14:37:02 +0530

    adding loged in page and password section and other things in config file

--- app/config/passport.js
+++ app/config/passport.js
@@ -1,36 +1,42 @@
-const LocalStrategy = require('passport-local').Strategy
-const User = require('../models/user')
-const bcrypt = require('bcrypt')    
+const LocalStrategy = require('passport-local').Strategy; // Importing LocalStrategy from passport-local module
+const User = require('../models/user'); // Importing User model
+const bcrypt = require('bcrypt'); // Importing bcrypt for password hashing
 
 function init(passport) {
     passport.use(new LocalStrategy({ usernameField: 'email' }, async (email, password, done) => {
-        // Login
-        // check if email exists
-        const user = await User.findOne({ email: email })
-        if(!user) {
-            return done(null, false, { message: 'No user with this email' })
+        // Login authentication process
+        // Check if email exists in the database
+        const user = await User.findOne({ email: email });
+        if (!user) {
+            // If no user found with the provided email
+            return done(null, false, { message: 'No user with this email' });
         }
 
+        // Compare the provided password with the hashed password stored in the database
         bcrypt.compare(password, user.password).then(match => {
-            if(match) {
-                return done(null, user, { message: 'Logged in succesfully' })
+            if (match) {
+                // If password matches
+                return done(null, user, { message: 'Logged in successfully' });
             }
-            return done(null, false, { message: 'Wrong username or password' })
+            // If password doesn't match
+            return done(null, false, { message: 'Wrong username or password' });
         }).catch(err => {
-            return done(null, false, { message: 'Something went wrong' })
-        })
-    }))  
+            // Error handling for bcrypt comparison
+            return done(null, false, { message: 'Something went wrong' });
+        });
+    }));
 
+    // Serialize user object to store in session
     passport.serializeUser((user, done) => {
-        done(null, user._id)
-    })
+        done(null, user._id); // Storing user's MongoDB _id in the session
+    });
 
+    // Deserialize user object from session
     passport.deserializeUser((id, done) => {
         User.findById(id, (err, user) => {
-            done(err, user)
-        })
-    })
-
+            done(err, user); // Retrieving user object based on _id from the session
+        });
+    });
 }
 
-module.exports = init
+module.exports = init; // Exporting the initialization function for passport
